c71f0a56859414656a5b6dc0e3118f401ee36db9
sharma1208 <90787231+sharma1208@users.noreply.github.com>
2021-09-23 11:59:24 -0500

    CarDealership

--- Unit3_Boolean/homework/CarDealershipLab.java
+++ Unit3_Boolean/homework/CarDealershipLab.java
@@ -1,5 +1,20 @@
 package Unit3_Boolean.homework;
 
 public class CarDealershipLab {
+       //Vehicle instance variables, details
+        private int year;
+        private int mileage;
+        private double fuelEfficiency;
+        private int price;
+        private String model;
+        private String color;
+        private boolean luxuryPackageStatus;
+        private boolean a4WDStatus;
+        private boolean sportsPackageStatus;
+
+        //Constant
+        final String VEHICLE_MAKE = "Chevrolet";
+        final double TAX_RATE = 12.2;
+
 
 }
