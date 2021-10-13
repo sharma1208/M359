dd4c051a150fe3a8b833620376fbf4c107f14f91
sharma1208 <90787231+sharma1208@users.noreply.github.com>
2021-10-13 12:00:18 -0500

    challenge probs

---
+++ Unit4_Iteration/examples/Notes44CodeAnalysis.java
@@ -0,0 +1,39 @@
+package Unit4_Iteration.examples;
+
+public class Notes44CodeAnalysis {
+    public static void main(String[] args) {
+        for(int i = 5; i > 0;  i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        // Challenge 2
+        for(int i = 5; i > 0;  i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    //Challenge 3
+    everyOtherLetterPyramid("FremdVikings");
+
+}
+
+    public static void everyOtherLetterPyramid(String letters){
+        String current  = letters;
+        System.out.println(current);
+        while(current.length() > 1){
+            String updated = "";
+            for(int i = 0; i < current.length(); i +=2){
+                updated += letters.substring(i,i+1);
+            }
+            current = updated;
+            System.out.println(current);
+        }
+    }
+
+
