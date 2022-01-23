56693f59c3e24c389eb867007797d340fd4d2461
sharma1208 <90787231+sharma1208@users.noreply.github.com>
2022-01-23 14:01:30 -0600

    input mismatch try/catch block

--- Unit7ArrayLists/examples/TicketMasterDriver.java
+++ Unit7ArrayLists/examples/TicketMasterDriver.java
@@ -1,10 +1,12 @@
 package Unit7ArrayLists.examples;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
 public class TicketMasterDriver {
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException {
         File myFile = new File("showData.txt");
         Scanner fileIn = new Scanner(myFile);
         TicketMaster uploadFile = new TicketMaster(fileIn);
@@ -15,24 +17,46 @@ public class TicketMasterDriver {
         uploadFile.printList(uploadFile.getShowList());
 
         Scanner keyboardInputs = new Scanner(System.in);
-        boolean notQuit = true;
-        while(notQuit) {
-            System.out.println("Pick one of the options below by inputting the number that correspond to the action");
-            System.out.println("1. Sort A-Z");
-            System.out.println("2. Sort Z-A");
-            System.out.println("3. Sort price high-low");
-            System.out.println("4. Sort price low-high");
-            System.out.println("5. Search by city");
-            System.out.println("6. Quit");
-            try{
+        boolean keepGoing = true;
+        while (keepGoing){
+
+            try {
+                System.out.println("Pick one of the options below by inputting the number that correspond to the action");
+                System.out.println("1. Sort A-Z");
+                System.out.println("2. Sort Z-A");
+                System.out.println("3. Sort price high-low");
+                System.out.println("4. Sort price low-high");
+                System.out.println("5. Search by city");
+                System.out.println("6. Quit");
+
                 int numberExecute = keyboardInputs.nextInt();
 
+                if (numberExecute > 6 || numberExecute < 1) {
+
+                    System.out.println("Integer out of range, enter new integer");
+                }
+
+                if (numberExecute == 6) {
+                    keepGoing = false;
+                }
             }
-            keyboardInputs.nextLine();
+
+            catch(InputMismatchException e) {
+
+                System.out.println("Please enter an integer");
+                keyboardInputs.nextLine();
+
+            }
+
+
+
+
+
+
 
         }
 
 
+        }
 
     }
-}
