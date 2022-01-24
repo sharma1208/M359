f1d462b80a155c2e49c93283ff1fbe2c358667b4
sharma1208 <90787231+sharma1208@users.noreply.github.com>
2022-01-24 11:59:22 -0600

    yay

---
+++ Unit7ArrayLists/notesFor75/Item.java
@@ -0,0 +1,63 @@
+package Unit7ArrayLists.notesFor75;
+
+public class Item {
+    private String name;
+    private double cost;
+    private int quantity;
+
+    public Item(String name, double cost, int quantity) {
+        this.name = name;
+        this.cost = cost;
+        this.quantity = quantity;
+    }
+
+    /** This method determines if two items are equal
+     *  For two items to be equal they will have the same name and cost
+     *  The quantity doesn't matter
+     */
+    public boolean equals(Item other) {
+        if(other.getName().equals(this.getName()) && other.getCost() == this.getCost()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * this method returns a String representation of a single Item
+     * The String format is similar to: Ice Cream, $3.99, 10
+     * Where 10 represents the quantity
+     */
+    public String toString() {
+        String output = name + ", $" + cost + ", " + quantity;
+        return output;
+    }
+
+
+    // ALL GETTER AND SETTER METHODS FOR INSTANCE VARIABLES
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
